18eca553482438472cecae9158b4363cd47cb732
Petemc121 <petemc121@gmail.com>
2021-11-30 14:46:15 +0000

    Heroku

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const cors = require("cors");
 const importedPool = require("./database");
-
 const app = express();
 const PORT = process.env.PORT || 5000;
 const path = require("path");
