b95f5c88462208ae711b2f33b83205dc160008ed
Petemc121 <petemc121@gmail.com>
2021-12-01 15:03:01 +0000

    Heroku

--- server.js
+++ server.js
@@ -104,5 +104,3 @@ app.get("*", (req, res) => {
 app.listen(PORT, () => {
   console.log(`Server is starting on port ${PORT}`);
 });
-
-//"heroku-postbuild": "cd client && npm install && npm run build",
